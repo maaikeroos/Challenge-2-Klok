d3bdb9e1adbbe91b8a1954a20e822d7bffd4bdb0
maaikeroos <61278422+maaikeroos@users.noreply.github.com>
2020-03-25 22:28:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ function realtimeClock(){
 	//zet de urencomponent om in 24 urenformaat
 	var hours = (hours24 > 12) ? hours24 - 12 : hours24;
 
-	//
 	hours = ("0" + hours).slice(-2);
 	minutes = ("0" + minutes).slice(-2);
 	seconds = ("0" + seconds).slice(-2);
